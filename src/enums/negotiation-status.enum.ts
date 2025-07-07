70f1c2d9da291f9aa5988794f46a074078f16071
Douglas <dellonath@gmail.com>
2025-07-06 21:44:08 -0300

    feat/adding negotiation status enum

---
+++ src/enums/negotiation-status.enum.ts
@@ -0,0 +1,9 @@
+export enum negotiationStatus {
+  WAITING_ATTENDANCE = 'waiting_attendance',
+  IN_NEGOTIATION = 'in_negotiation',
+  WAITING_RETURN = 'waiting_return',
+  WAITING_DOCS = 'waiting_docs',
+  NOT_INTERESTED = 'not_interested',
+  PROPOSAL_SENT = 'proposal_sent',
+  PAID = 'paid'
+}
