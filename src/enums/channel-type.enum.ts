099bb8599b9b288fef4a2a7f14cfce16f2ed728b
Douglas <dellonath@gmail.com>
2025-07-06 21:43:30 -0300

    feat/removing both value for channel type

--- src/enums/channel-type.enum.ts
+++ src/enums/channel-type.enum.ts
@@ -1,6 +1,5 @@
 // Indicate if the client/user is related to either online, offline or both channel
 export enum channelType {
   ONLINE = 'online',
-  OFFLINE = 'offline',
-  BOTH = 'both'
+  OFFLINE = 'offline'
 }
