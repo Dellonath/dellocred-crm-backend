9f8a92cb1655b087535671097e5247d05f7db944
Douglas <dellonath@gmail.com>
2025-07-10 22:04:27 -0300

    feat/allowing all in cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: process.env.FRONTEND_ORIGIN,
+    origin: '*',
   });
   await app.listen(process.env.PORT ?? 3000);
 }
